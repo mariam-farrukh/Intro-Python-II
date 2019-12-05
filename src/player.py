7b140cf87453f5bdb5892a6254da50f269626140
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-12-05 12:48:45 -0600

    Added item steps to player and room class

--- src/player.py
+++ src/player.py
@@ -5,6 +5,35 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
+
+    def add_item_in(self, item):
+        self.inventory.append(item)
+        print(f"Added {item.name} to inventory")
+
+    def remove_item_in(self, item_name):
+        for item in self.inventory:
+            if item.name.lower() == item_name.lower():
+                self.inventory.remove(item)
+                print(f"You dropped {item.name}")
+                return item
+            else:
+                return None
+
+    def list_inventory(self):
+        if self.inventory == []:
+            print("Inventory: Empty")
+        else:
+            inventory = ", ".join([item.name for item in self.inventory])
+            print(f"Inventory: {inventory}")
+
+    def item_locate(self, item_name):
+        for item in self.inventory:
+            if item.name.lower() == item_name.lower():
+                return item
+            else:
+                return None
+
     def player_move(self, direction):
         str = f"{direction}_to"
         new_room = getattr(self.current_room, str)
--- src/room.py
+++ src/room.py
@@ -5,12 +5,37 @@ class Room:
     '''
     Room class stores name, description, and direction
     '''
-    def __init__(self, name, description):
+    def __init__(self, name, description, *item_list):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.list = []
+        if len(item_list) > 0:
+            for item in item_list:
+                self.list.append(item)
+
+    def add_item(self, item):
+        self.list.append(item)
+
+    def delete_item(self, item):
+        self.list.remove(item)
+
+    def item_locate(self, item_name):
+        for item in self.list:
+            if item.name.lower() == item_name.lower():
+                return item
+            else:
+                return None
+
+    def items_avail(self):
+        if self.list == []:
+            print("You look around and see no items here.")
+        else:
+            for item in self.list:
+                print(f"You search the room and find: {item.name}")
+
     def __str__(self):
         return f"Location: {self.name} \n Description: {self.description}"
